a3ff07d80b2949e099fb96632e973c00f4007d8b
João Victor <ruasjoaovictorsantos@gmail.com>
2024-10-28 08:20:09 -0300

    Alterei a pasta do arquivo css "saude.css" agora ela está na pasta "assets"

--- saude.php
+++ saude.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Saúde e Bem-Estar</title>
-  <link rel="stylesheet" href="saude.css">
+  <link rel="stylesheet" href="assets\css\saude.css">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
