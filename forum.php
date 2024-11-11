85b7eb7dd96fe6e383c9986ac20fa9363bdd6c98
Pedro Gabriel (fã do manoel gomes) <pedrogabrielxx268@gmail.com>
2024-11-11 10:50:39 -0300

    title forum arrumado

--- forum.php
+++ forum.php
@@ -122,7 +122,7 @@ if ($id) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fórum - RigRover</title>
+    <title>Fórum - IncludeGen</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/forum.css">
